4247d062630af294bcb47830fe093f84b4b5da42
cocojambo007 <aventonyui@gmail.com>
2023-05-15 15:50:40 +0300

    добавил файлы для jenkins

--- test_ui.py
+++ test_ui.py
@@ -1,5 +1,5 @@
 from selene import browser
-
+import allure
 from pages.login_page import login_page
 from pages.user_page import user_page
 from pages.artist_page import artist_page
@@ -10,14 +10,14 @@ from pages.footer_page import footer_page
 
 band = 'Electric Wizard'
 
-
+@allure.step('Открыть главную страницу')
 def test_login():
     browser.open('https://www.last.fm/login')
     login_page.input_username_or_email('cocojambo_qa')
     login_page.input_password('1@qwerty')
     login_page.press_button_login()
 
-
+@allure.step('Открыть главную страницу')
 def test_search_band():
     test_login()
     user_page.click_search_toggle()
@@ -25,12 +25,12 @@ def test_search_band():
     user_page.click_search_submit()
     search_page.click_found_artist(band)  # лучше перенести в search_page
 
-
+@allure.step('Открыть главную страницу')
 def test_like_song():
     test_search_band()
     artist_page.like_song()
 
-
+@allure.step('Открыть главную страницу')
 def test_new_playlist():
     test_login()
     user_page.click_playlist()
@@ -40,13 +40,13 @@ def test_new_playlist():
     playlist_page.search_submit_button()
     playlist_page.choice_track_for_playlist()
 
-
+@allure.step('Открыть главную страницу')
 def test_dislike_song():
     test_login()
     user_page.click_loved_track()
     user_page.dislike_song_from_loved_track()
 
-
+@allure.step('Открыть главную страницу')
 def test_upload_picture_profile():
     test_login()
     user_page.click_add_image()
@@ -55,14 +55,14 @@ def test_upload_picture_profile():
     settings_page.click_delete_picture_button()
     settings_page.click_save_changes()
 
-
+@allure.step('Открыть главную страницу')
 def test_change_display_name():
     test_login()
     footer_page.footer_settings()
     settings_page.change_display_name('Pavel Fomin')
     settings_page.click_save_changes()
 
-
+@allure.step('Открыть главную страницу')
 def test_change_country():
     test_login()
     footer_page.footer_settings()
@@ -70,14 +70,14 @@ def test_change_country():
     settings_page.pick_country()
     settings_page.click_save_changes()
 
-
+@allure.step('Открыть главную страницу')
 def test_change_website():
     test_login()
     footer_page.footer_settings()
     settings_page.change_website()
     settings_page.click_save_changes()
 
-
+@allure.step('Открыть главную страницу')
 def test_change_about_you():
     test_login()
     footer_page.footer_settings()
