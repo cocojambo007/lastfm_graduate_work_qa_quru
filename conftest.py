f8c39db8543799e4cefcdbc881788e3ec01e891e
cocojambo007 <aventonyui@gmail.com>
2023-05-17 09:21:33 +0300

    изменил driver.implicitly_wait

--- conftest.py
+++ conftest.py
@@ -32,7 +32,7 @@ def setup_browser(request):
         options=options)
 
     browser.config.driver = driver
-    driver.implicitly_wait(240)
+    driver.implicitly_wait(120)
     yield
     add_logs(browser)
     add_screenshot(browser)
