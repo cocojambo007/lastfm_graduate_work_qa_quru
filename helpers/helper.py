884b4edf65ec719a37941130da267adff652b3fe
cocojambo007 <aventonyui@gmail.com>
2023-05-23 13:55:26 +0300

    полировка

--- helpers/helper.py
+++ helpers/helper.py
@@ -1,5 +1,9 @@
 from requests import Session
+import json
+import logging
 
+import allure
+import curlify
 
 class BaseSession(Session):
     def __init__(self, **kwargs):
@@ -8,4 +12,27 @@ class BaseSession(Session):
 
     def request(self, method, url, **kwargs):
         response = super().request(method, url=f'{self.base_url}{url}', **kwargs)
+        message = curlify.to_curl(response.request)
+        logging.info(f'{response.status_code} {message}')
+        with allure.step(f'{response.request.method} {response.request.url}'):
+            allure.attach(
+                body=message.encode('utf8'),
+                name=f'Request {response.request.method} {response.status_code}',
+                attachment_type=allure.attachment_type.TEXT,
+                extension='txt'
+            )
+            try:
+                allure.attach(
+                    body=json.dumps(response.json(), indent=4, ensure_ascii=False).encode('utf8'),
+                    name=f'Response {response.request.method}',
+                    attachment_type=allure.attachment_type.JSON,
+                    extension='json'
+                )
+            except ValueError as error:
+                allure.attach(
+                    body=response.text.encode('utf8'),
+                    name=f'NOT Json Response {response.request.method}',
+                    attachment_type=allure.attachment_type.JSON,
+                    extension='json'
+                )
         return response
